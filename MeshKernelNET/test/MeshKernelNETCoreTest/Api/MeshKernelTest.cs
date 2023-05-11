4749d9d2534d083aa5aa6488840b06d2ca2e8909
Luca Carniato <luca.carniato@deltares.nl>
2023-05-11 16:33:16 +0200

    Add unit test

--- MeshKernelNET/test/MeshKernelNETCoreTest/Api/MeshKernelTest.cs
+++ MeshKernelNET/test/MeshKernelNETCoreTest/Api/MeshKernelTest.cs
@@ -1519,5 +1519,38 @@ namespace MeshKernelNETCoreTest.Api
             }
         }
 
+        [Test]
+        public void GetSelectedVerticesInPolygonThroughAPI()
+        {
+            using (var mesh = GenerateRegularGrid(4, 4, 1, 1))
+            using (var api = new MeshKernelApi())
+            {
+                var id = 0;
+                try
+                {
+                    id = api.AllocateState(0);
+
+                    Assert.IsTrue(api.Mesh2dSet(id, mesh));
+
+                    var geometryListIn = new DisposableGeometryList();
+
+                    geometryListIn.XCoordinates = new[] {  1.5, 1.5, 3.5, 3.5, 1.5 };
+                    geometryListIn.YCoordinates = new[] { -1.5, 1.5, 1.5,-1.5,-1.5 };
+                    geometryListIn.Values = new[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+                    geometryListIn.NumberOfCoordinates = geometryListIn.XCoordinates.Length;
+                    geometryListIn.GeometrySeparator = api.GetSeparator();
+
+
+                    int inside = 0;
+                    var selectedVertices = api.GetSelectedVerticesInPolygon(id, ref geometryListIn, inside);
+                    Assert.AreEqual(selectedVertices.Length, 12);
+                }
+                finally
+                {
+                    api.DeallocateState(id);
+
+                }
+            }
+        }
     }
 }

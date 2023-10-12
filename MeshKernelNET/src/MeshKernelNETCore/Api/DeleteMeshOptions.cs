87f379a3d40067ea4972f6aa8aa91a8a73183232
Ahmad El Sayed <ahmad.elsayed@deltares.nl>
2023-10-12 15:56:14 +0200

    Enumerator had wrong descriptions

--- MeshKernelNET/src/MeshKernelNETCore/Api/DeleteMeshOptions.cs
+++ MeshKernelNET/src/MeshKernelNETCore/Api/DeleteMeshOptions.cs
@@ -4,10 +4,10 @@ namespace MeshKernelNETCore.Api
 {
     public enum DeleteMeshOptions
     {
-        [Description("Inside the polygon and intersecting with it")]
+        [Description("Mesh inside the polygon and not intersecting with it")]
         InsideNotIntersected = 0,
 
-        [Description("Inside the polygon and not intersecting with it")]
+        [Description("Mesh inside the polygon and intersecting with it")]
         InsideAndIntersected = 1
     }
 }

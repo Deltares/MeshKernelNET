58b336d1f2aacb5c3839aa6836285f596335bc08
Ahmad El Sayed <ahmad.elsayed@deltares.nl>
2023-12-06 10:22:21 +0100

    Conform to proj struct of main branch to resolve path issues in TC

---
+++ src/MeshKernelNETCore/Api/StringBufferSizeAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("MeshKernelNETCoreTest")]
+
+namespace MeshKernelNETCore.Api
+{
+    internal class StringBufferSizeAttribute : Attribute
+    {
+        public int BufferSize { get; set; }
+    }
+}

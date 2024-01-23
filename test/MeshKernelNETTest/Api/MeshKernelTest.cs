9c03585a50d55f7c5d88a9a9deb7a0f39836638b
Luca Carniato <luca.carniato@deltares.nl>
2024-01-23 11:14:06 +0100

    Update meshkernel nugget Fix unit test

--- test/MeshKernelNETTest/Api/MeshKernelTest.cs
+++ test/MeshKernelNETTest/Api/MeshKernelTest.cs
@@ -2381,7 +2381,7 @@ namespace MeshKernelNETTest.Api
                     Assert.AreEqual(0, api.Mesh2dMakeGlobal(id, 19,25));
 
                     Assert.AreEqual(0, api.Mesh2dGetData(id, out mesh2d));
-                    Assert.AreEqual(1200, mesh2d.NumEdges);
+                    Assert.AreEqual(1233, mesh2d.NumEdges);
                     Assert.AreEqual(629, mesh2d.NumNodes);
                 }
                 finally
